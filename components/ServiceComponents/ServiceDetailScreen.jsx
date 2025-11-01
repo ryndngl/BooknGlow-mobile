222a6192114cc047846b4c6179200c1ada8ea4cb
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-11-02 04:45:15 +0800

    chore(mobile): Remove redundant comment from ServiceDetailScreen imports

--- components/ServiceComponents/ServiceDetailScreen.jsx
+++ components/ServiceComponents/ServiceDetailScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, RefreshControl } from 'react-native'; // ✅ ADD RefreshControl
+import { View, ScrollView, StyleSheet, RefreshControl } from 'react-native'; 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Import custom hook
