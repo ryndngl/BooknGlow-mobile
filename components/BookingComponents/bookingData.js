399e268b0f152960c95c6e2f8447ad1d7647d2e5
Ryan Laurence Dingle <ryanlaurencedingle@gmail.com>
2025-11-01 20:53:11 +0800

    Add a new time for booking in bookingData.js

--- components/BookingComponents/bookingData.js
+++ components/BookingComponents/bookingData.js
@@ -17,6 +17,14 @@ export const timeSlots = [
   "4:00 PM",
   "4:30 PM",
   "5:00 PM",
+  "5:30 PM",
+  "6:00 PM",
+  "6:30 PM",
+  "7:00 PM",
+  "7:30 PM",
+  "8:00 PM",
+  "8:30 PM",
+  "9:00 PM",
 ];
 
 // All available services
